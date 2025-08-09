8f97a4deae723ceb4ee6ffb705838f948237d20d
Adittoguy <adityasanap2001@gmail.com>
2025-08-09 12:51:53 +0530

    Add files via upload

---
+++ real.c
@@ -0,0 +1,21 @@
+//print area of square
+// #include <stdio.h>
+// int main() {
+//     int length=5;
+//     int area = length * length;
+//     printf("area of square is %d", area);
+//     return 0;
+// }
+
+//print circumference of circle
+# include <stdio.h>
+
+int main()
+{
+    int radius = 8;
+    float pi = 3.14;
+    float circumference = 2 * pi * radius;
+    printf("\ncircumference of circle is %f\n", circumference);
+    return 0;
+
+}
